0e8f99578a857516da46895816701438c160dc38
Johannes Pessi <pessijoh@arcada.fi>
2024-11-12 13:58:20 +0200

    uupg1

---
+++ Assets/Scripts/svagningsrorelse.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class svagningsrorelse : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    
+    Vector3 initialPosition;
+    float swayAmount = 0.0015f; 
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+    }
+
+    void Update()
+    {
+        
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            
+            float sway = Mathf.Sin(Time.time + vertices[i].x + vertices[i].z) * swayAmount;
+            
+            
+            vertices[i] = new Vector3(vertices[i].x, vertices[i].y + sway, vertices[i].z);
+        }
+
+      
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+
+        
+        transform.position = initialPosition;
+    }
+}
